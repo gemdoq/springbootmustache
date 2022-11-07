ed91cca424789b899f8d51ccf44a26916f3d035e
gemdoq <gemdoq@gmail.com>
2022-11-07 14:13:46 +0900

    ArticleController & form tag new.mustache add

--- src/main/java/com/mustache/bbs/controller/MustacheController.java
+++ src/main/java/com/mustache/bbs/controller/MustacheController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class MustacheController {
     @GetMapping(value = {"/hi", "/hi/{id}"})
-    public String mustacheConWithPathVariable(Model model, @PathVariable(value = "id", required = false) String id) {
+    public String mustacheConWithPathVariable(
+            Model model, @PathVariable(value = "id", required = false) String id) {
         model.addAttribute("username", "choi");
         if (id == null) {
             model.addAttribute("id", "");
