9a70576bf5b88f6cc00b8c667221c70202d83b35
gemdoq <gemdoq@gmail.com>
2022-11-07 14:36:39 +0900

    add ArticleController&ArticleDto&new.mustache

--- src/main/java/com/mustache/bbs/domain/dto/ArticleDto.java
+++ src/main/java/com/mustache/bbs/domain/dto/ArticleDto.java
@@ -1,18 +1,14 @@
 package com.mustache.bbs.domain.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 
 @Getter
+@AllArgsConstructor
 @ToString
 public class ArticleDto {
     private Long id;
     private String title;
     private String content;
-
-    public ArticleDto(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
 }
