d03345b90d97d1a60c9c9e74cc4aedeb5e6291f2
gemdoq <gemdoq@gmail.com>
2022-11-07 11:46:22 +0900

    added MustacheController and .mustache

---
+++ src/main/java/com/mustache/bbs/controller/MustacheController.java
@@ -0,0 +1,15 @@
+package com.mustache.bbs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class MustacheController {
+    @GetMapping(value = "/hi")
+    public String mustacheCon(Model model) {
+        model.addAttribute("username", "rok");
+        return "greetings";
+    }
+}
